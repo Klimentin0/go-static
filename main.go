080952509cae33958b75542dfec50413173c8bce
Klimentin0 <meshkov.klim@gmail.com>
2026-03-06 00:40:26 +0300

    setup for testing

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import fmt
+import (
+	"log"
+	"net/http"
+)
 
+// basic serving
 func main() {
-
+	fs := http.FileServer(http.Dir("./public"))
+	http.Handle("/", fs)
+	log.Println("running on :8090")
+	err := http.ListenAndServe(":8090", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
